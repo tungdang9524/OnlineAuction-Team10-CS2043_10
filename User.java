342768abbdd64ca4b66e8351d7b3cdb8a59b34f2
DangSonTung <tungdang9524@gmail.com>
2026-03-26 18:09:03 +0700

    feat: Add constructor to User.java

--- User.java
+++ User.java
@@ -8,5 +8,17 @@ abstract class User {
     protected String role;
     protected LocalDateTime createdAt;
     protected boolean isActive;
+    protected double balance;
+
+    public User(String id, String username, String password, String email, String role, LocalDateTime createdAt, boolean isActive, double balance) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+        this.email = email;
+        this.role = role;
+        this.createdAt = createdAt;
+        this.isActive = isActive;
+        this.balance = balance;
+    }
 
 }
