de4ec4a7ac3632f37dce6c1428090ef5cc2865c4
DangSonTung <tungdang9524@gmail.com>
2026-03-26 22:54:17 +0700

    Add getter/setter for all credentials. Set all to private

--- src/model/User.java
+++ src/model/User.java
@@ -1,20 +1,22 @@
 package model;
+
 import java.time.LocalDateTime;
 
-abstract class User {
-    protected String id;
-    protected String username;
-    protected String passwordHash;
-    protected String fullName;
-    protected String email;
-    protected String role;
-    protected LocalDateTime createdAt;
-    protected String status;
+public abstract class User {
+    private String id;
+    private String username;
+    private String passwordHash;
+    private String fullName;
+    private String email;
+    private String role;
+    private LocalDateTime createdAt;
+    private String status;
 
-    public User(String id, String username, String passwordHash, String email, String role, LocalDateTime createdAt, String status) {
+    public User(String id, String username, String passwordHash, String fullName, String email, String role, LocalDateTime createdAt, String status) {
         this.id = id;
         this.username = username;
         this.passwordHash = passwordHash;
+        this.fullName = fullName;
         this.email = email;
         this.role = role;
         this.createdAt = createdAt;
@@ -29,12 +31,67 @@ abstract class User {
         this.id = id;
     }
 
+    public String getUsername() {
+        return this.username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPasswordHash() {
+        return this.passwordHash;
+    }
+
+    public void setPasswordHash(String passwordHash) {
+        this.passwordHash = passwordHash;
+    }
+
+    public String getFullName() {
+        return this.fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getEmail() {
+        return this.email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getRole() {
+        return this.role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return this.createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public String getStatus() {
+        return this.status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
     public void login() {
-        System.out.println(this.username + "logged in.");
+        System.out.println(this.username + " logged in.");
     }
 
     public void logout() {
-        System.out.println(this.username + "logged out.");
+        System.out.println(this.username + " logged out.");
     }
-
 }
