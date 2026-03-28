3a55a3f5e20920490effa2e9a04e2c0060186143
LongSlyP <longhuyentoan@gmail.com>
2026-03-28 12:54:00 +0700

    Thừa Roll role , ở super gán Role.SELLER , Role.Bidder,Roll.Admin luôn

--- src/model/Admin.java
+++ src/model/Admin.java
@@ -3,7 +3,7 @@ package model;
 import java.time.LocalDateTime;
 
 public class Admin extends User {
-    public Admin(String id, String username, String passwordHash, String fullName, String email, Role role, LocalDateTime createdAt, UserStatus userStatus) {
+    public Admin(String id, String username, String passwordHash, String fullName, String email,LocalDateTime createdAt, UserStatus userStatus) {
         super(id, username, passwordHash, fullName, email, Role.ADMIN, createdAt, UserStatus.ACTIVE);
     }
 }
